c9b7d866bafb323639f6932fc1f91eafba5b8239
OhRyungyi <giligoni120515@gmail.com>
2020-06-12 17:59:34 +0900

    layout control

--- app/src/main/java/com/example/ibm_project_fin/mapActivity.kt
+++ app/src/main/java/com/example/ibm_project_fin/mapActivity.kt
@@ -7,12 +7,12 @@ class mapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-
+/*
         //데이터 전송받기
         var intent=intent
         var visitedStoreList=intent.extras?.getSerializable("data") as ArrayList<StoreData>
         var output=intent.extras?.getSerializable("store") as StoreData
-
+*/
 
 
     }
